fc36a4011ffb3a4ffd2580dbc4ba28863fe3187b
Aditya Parihar <adityparihar5050@gmail.com>
2022-09-22 11:24:29 +0530

    Binary Search Rotated in Rotated array solved 💯💯💯💯

---
+++ StriverSortedArray.java
@@ -0,0 +1,44 @@
+public class StriverSortedArray
+{
+public static void main(String[] args)
+{
+    int[] arr = {4,5,6,7,0,1,2};
+  System.out.println(elementSearch(arr,10 ));
+    
+}
+public static int elementSearch(int[] array,int target)
+{
+ int start = 0 ;
+ int end = array.length-1;
+ while (start<=end)
+ {
+   int mid = start+ (end-start)/2;
+   
+   if (array[mid] == target) return mid; 
+
+  if (array[start]<=array[mid]) 
+  {
+    if (array[start] <= target && array[mid]>=target) 
+    {
+      end = mid-1;    
+    } 
+    else
+    {
+        start = mid+1;
+    }  
+  } 
+  else
+  {
+    if (array[end]>=target && array[mid]<=target) 
+    {
+      start = mid+1;    
+    }
+    else 
+    {
+     end = mid-1;    
+    }  
+  }
+ } 
+ return -1;  
+}    
+}
