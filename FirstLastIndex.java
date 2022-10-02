591b1f2cde4690db6a1ffd4fb4f44152f1f37506
Aditya Parihar <adityparihar5050@gmail.com>
2022-10-02 17:24:28 +0530

    First & Last Index of a element in array ✅✅

--- FirstLastIndex.java
+++ FirstLastIndex.java
@@ -10,25 +10,23 @@ public class FirstLastIndex
 {
 public static void main(String[] args) 
 {
-  int [] array = {5,7,7,2,8,8,10};
-  System.out.println(Arrays.toString(searchRange(array,2)));
+  int [] array = {5,2,8,8,10,5};
+  System.out.println(Arrays.toString(searchRange(array,5)));
 }
  public static int[] searchRange(int[] nums, int target)
  {
   int[] ans = {-1,-1} ;
-  ans[0] = search(nums, target, true);
-  ans[1] = search(nums, target, false);
+  ans[0] = firstSearch(nums, target);
+  ans[1] = lastSearch(nums, target);
   
   return ans ;
  }
-
-
- public static int search(int[] nums, int target, boolean startIndex)
+ public static int firstSearch(int[] nums, int target)
  {
-    int ans = -1 ;
-    
+    int ans = -1 ;        
     int start = 0 ;
     int end = nums.length-1;
+    
     while (start<=end) 
     {
       int mid = start + (end-start)/2;
@@ -36,16 +34,8 @@ public static void main(String[] args)
       if (target == nums[mid])
       {
         ans = mid;
-        if (startIndex) 
-          {
-            end = mid-1 ;  
-          } 
-          else 
-          {
-           start = mid+1 ;  
-          }
-      }
-      
+        end = mid-1;
+      }      
       else if(target<nums[mid]) 
       {
         end = mid-1 ;        
@@ -56,5 +46,31 @@ public static void main(String[] args)
       }
     }
     return ans ;
+ }
+ public static int lastSearch(int[] nums, int target)
+ {
+  int ans = -1 ;        
+  int start = 0 ;
+  int end = nums.length-1;
+  
+  while (start<=end) 
+  {
+    int mid = start + (end-start)/2;
+
+    if (target == nums[mid])
+    {
+      ans = mid;
+      start = mid+1;
+    }      
+    else if(target<nums[mid])  
+    {
+      end = mid-1 ;        
+    }
+    else
+    {
+      start = mid+1;
+    }
+  }
+  return ans ; 
  }
 }
